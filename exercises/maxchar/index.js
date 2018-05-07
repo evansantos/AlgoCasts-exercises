92b01e2394dc58550ad9275dd508f375e4763010
Evandro Santos <evan.its.me@gmail.com>
2018-05-06 21:15:02 -0300

    adding maxchar
    Add maxchar solution code, splitting in two differents functions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let chars = str
+    .split('')
+    .reduce((total, letter) => {
+      total[letter] ?
+        total[letter]++ :
+        total[letter] = 1;
+
+      return total;
+    }, {});
+
+  return higherChar(chars);
+}
+
+function higherChar(chars) {
+  let max = 0;
+  let maxChar = '';
+
+  for(let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
