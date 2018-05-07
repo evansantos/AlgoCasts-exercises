ecf8cf1fb1ac41c8ae062a41bd1b039bae447354
Evandro Santos <evan.its.me@gmail.com>
2018-05-07 13:57:35 -0300

    upgrading to sort with @guilhermegm

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -20,17 +20,11 @@ function maxChar(str) {
 }
 
 function higherChar(chars) {
-  let max = 0;
-  let maxChar = '';
-
-  for(let char in chars) {
-    if (chars[char] > max) {
-      max = chars[char];
-      maxChar = char;
+  return Object.entries(chars).sort(
+    (value, comparator) => {
+      return value[1] > comparator[1] ? -1 : 1;
     }
-  }
-
-  return maxChar;
+  )[0][0];
 }
 
 module.exports = maxChar;
